21065dea7a6edbddd31109209769b5ae0d929baa
legitkat <amikeight@gmail.com>
2021-03-09 22:07:16 +0530

    Added the code

--- array1.c
+++ array1.c
@@ -5,6 +5,25 @@
 #include<stdio.h>
 
 int main(){
+    int size = 0, avg = 0, sum = 0;
+    
+    printf("Enter the size of the array u want : ");
+    scanf("%d", &size);
+
+    int array[size];
+
+    //inputing the elements of the array
+    for(int i = 0; i < size; i++){
+        printf("Enter the %dth term : ", i+1);
+        scanf("%d", &array[i]);
+    }
+
+    for(int i = 0; i < size; i++)
+        sum += array[i];
+
+    avg = sum/size;
+
+    printf("The average of the elements of the array is %d\n", avg);
 
     return 0;
 }
